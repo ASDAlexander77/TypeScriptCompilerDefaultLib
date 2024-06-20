175207c70dcb529830d1aa5bc4bf7ee0ad2b56f5
Alex D <duzhar@googlemail.com>
2024-06-20 21:06:43 +0100

    update

--- scripts/build.sh
+++ scripts/build.sh
@@ -12,17 +12,19 @@ SRC=.
 OUTPUT=.
 
 ROOT=..
-export GC_LIB_PATH=$ROOT/TypeScriptCompiler/__build/gc/ninja/$BUILD
-export LLVM_LIB_PATH=$ROOT/TypeScriptCompiler/__build/llvm/ninja/$BUILD/lib
-export TSC_LIB_PATH=$ROOT/TypeScriptCompiler/__build/tsc/linux-ninja-gcc-$BUILD/lib
+BUILD_PATH=$ROOT/TypeScriptCompiler/__build
+BIN_PATH=$BUILD_PATH/tsc/linux-ninja-gcc-$BUILD/bin
+export GC_LIB_PATH=$BUILD_PATH/gc/ninja/$BUILD
+export LLVM_LIB_PATH=$BUILD_PATH/llvm/ninja/$BUILD/lib
+export TSC_LIB_PATH=$BUILD_PATH/tsc/linux-ninja-gcc-$BUILD/lib
 
 mkdir dll
 mkdir lib
 
 # Build DLL
-$ROOT/TypeScriptCompiler/__build/tsc/linux-ninja-gcc-$BUILD/bin/tsc --emit=dll $SRC/src/lib.ts -o $OUTPUT/dll/liblib.so
+$BIN_PATH/tsc --emit=dll $SRC/src/lib.ts -o $OUTPUT/dll/liblib.so
 
 # Build Lib
-$ROOT/TypeScriptCompiler/__build/tsc/linux-ninja-gcc-$BUILD/bin/tsc --emit=obj --export=none $SRC/src/lib.ts -o $OUTPUT/lib/lib.o
+$BIN_PATH/tsc --emit=obj --export=none $SRC/src/lib.ts -o $OUTPUT/lib/lib.o
 #ar rcs $OUTPUT/lib/lib.a $OUTPUT/lib/lib.o
 llvm-ar rcs $OUTPUT/lib/lib.a $OUTPUT/lib/lib.o
