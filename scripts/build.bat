c1d9131df2a029fb65e79f9ac7d3d4e3a2086d01
ASDAlexander77 <duzhar@googlemail.com>
2024-06-29 17:01:03 +0100

    improving update

--- scripts/build.bat
+++ scripts/build.bat
@@ -18,13 +18,13 @@ set ROOT=..\..
 set BUILD_PATH=%ROOT%\TypeScriptCompiler\__build
 set _3RD_PATH=%ROOT%\TypeScriptCompiler\3rdParty
 
-if not "%GC_LIB_PATH%"=="" (
+if "%GC_LIB_PATH%"=="" (
 	set GC_LIB_PATH=%BUILD_PATH%\gc\msbuild\%ARCH%\%BUILD%\Debug
 )
-if not "%LLVM_LIB_PATH%"=="" (
+if "%LLVM_LIB_PATH%"=="" (
 	set LLVM_LIB_PATH=%BUILD_PATH%\llvm\msbuild\%ARCH%\%BUILD%\Debug\lib
 )
-if not "%TSC_LIB_PATH%"=="" (
+if "%TSC_LIB_PATH%"=="" (
 	set TSC_LIB_PATH=%BUILD_PATH%\tsc\windows-msbuild-%BUILD%\lib
 )
 
@@ -37,7 +37,14 @@ rem Build DLL
 rem Build Lib
 %BUILD_PATH%\tsc\windows-msbuild-%BUILD%\bin\tsc.exe --emit=obj --export=none %SRC%\src\lib.ts -o %OUTPUT%\lib\lib.obj
 rem %_3RD_PATH%\llvm\%ARCH%\%LLVM_BUILD%\bin\llvm-lib.exe /out:%OUTPUT%\lib\TypeScriptDefaultLib.lib %OUTPUT%\lib\lib.obj
+
+for /f "usebackq tokens=*" %%i in (`vswhere -legacy -latest -property installationPath`) do (
+  set VSPATH="%%i\Common7\Tools\VsDevCmd.bat"
+)
+
+call %VSPATH%
 lib.exe /out:%OUTPUT%\lib\TypeScriptDefaultLib.lib %OUTPUT%\lib\lib.obj
+
 del %OUTPUT%\lib\lib.obj
 
 set BUILD_LIB_PATH=.\__build\%BUILD%\defaultlib
