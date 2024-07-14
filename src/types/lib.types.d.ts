e21b48050ac9996ab8e1ca477954db68b76b63d8
ASDAlexander77 <duzhar@googlemail.com>
2024-07-14 23:43:01 +0100

    added native types

--- src/types/lib.types.d.ts
+++ src/types/lib.types.d.ts
@@ -1,2 +1,16 @@
-type int = TypeOf<1>;
+type byte = TypeOf<Int8Array[0]>;
+type int = TypeOf<Int32Array[0]>;
+// bigint is used as keyword
 type char = TypeOf<""[0]>;
+type i16 = TypeOf<Int16Array[0]>;
+type i32 = TypeOf<Int32Array[0]>;
+type i64 = TypeOf<BigInt64Array[0]>;
+type u16 = TypeOf<Uint16Array[0]>;
+type u32 = TypeOf<Uint32Array[0]>;
+type u64 = TypeOf<BigUint64Array[0]>;
+type f16 = TypeOf<Float16Array[0]>;
+type f32 = TypeOf<Float32Array[0]>;
+type f64 = TypeOf<Float64Array[0]>;
+type half = TypeOf<Float16Array[0]>;
+type float = TypeOf<Float32Array[0]>;
+type double = TypeOf<Float64Array[0]>;
