1d6962b7197f08c5c2ca8e6d852dab4772fff292
ASDAlexander77 <duzhar@googlemail.com>
2024-07-27 00:19:07 +0100

    some fixes

--- src/generics/lib.generics.ts
+++ src/generics/lib.generics.ts
@@ -1,27 +1,11 @@
 /// <reference path="native/lib.native.d.ts" />
 
-namespace __Array {
-    function at<T>(this: T[], index: int): T {
+static class Array<T> {
+    public at(this: T[], index: int) {
         return this[index];
     }
 
-    function concat<T>(this: T[], ...other: T[][]): T[] {
-        let count = this.length;
-        for (const item of other)
-            count += item.length;        
-        let newArray = new Array<T>(count);
-        let index = 0;
-        memcpy(<Opaque>ReferenceOf(newArray[index]), <Opaque>ReferenceOf(this[0]), sizeof(T) * this.length);
-        index += this.length;
-        for (const item of other) {
-            memcpy(<Opaque>ReferenceOf(newArray[index]), <Opaque>ReferenceOf(item[0]), sizeof(T) * item.length);        
-            index += item.length;
-        }
-
-        return newArray;
-    }    
-
-    function copyWithin<T>(this: T[], target: int, start: int, end?: int = this.length - 1 ): T[] {
+    public copyWithin(this: T[], target: int, start: int, end?: int = this.length) {
 
         if (target < 0) {
             if (-this.length <= target) {
@@ -31,7 +15,7 @@ namespace __Array {
             }
         } else if (target >= this.length) {
             return this;
-        }        
+        }
 
         if (start < 0) {
             if (-this.length <= start) {
@@ -41,7 +25,7 @@ namespace __Array {
             }
         } else if (start >= this.length) {
             return this;
-        }    
+        }
 
         if (end < 0) {
             if (-this.length <= end) {
@@ -50,23 +34,54 @@ namespace __Array {
                 end = 0;
             }
         } else if (end >= this.length) {
-            return this;
-        }    
+            end = this.length;
+        }
 
-        memmove(<Opaque>ReferenceOf(this[target]), <Opaque>ReferenceOf(this[start]), sizeof(T) * (end - start + 1));
+        memmove(<Opaque>ReferenceOf(this[target]), <Opaque>ReferenceOf(this[start]), sizeof(T) * (end - start));
         return this;
-    }     
+    }
 
-    function toString<T>(this: T[]): string {
+    public *entries(this: T[]) {
+        for (let i = 0; i in this; i++) {
+            yield [i, this[i]];
+        }
+    }
+
+    public toString(this: T[]) {
         let str = "";
         for (const item of this) {
             if (str.length > 0)
                 str += ",";
-            str += item;        
+            str += item;
         }
 
         return str;
-    }    
+    }
+
+    public every(this: T[], func: (v: T) => boolean) {
+        let result = true;
+        for (const v of this) if (!(result &&= func(v))) break;
+        return result;
+    }
+}
+
+namespace __Array {
+    // to prevent cycling initialization of T[][] as Array<Array<....>> which causes inf. cycle
+    function concat<T>(this: T[], ...other: T[][]) {
+        let count = this.length;
+        for (const item of other)
+            count += item.length;
+        let newArray = new Array<T>(count);
+        let index = 0;
+        memcpy(<Opaque>ReferenceOf(newArray[index]), <Opaque>ReferenceOf(this[0]), sizeof(T) * this.length);
+        index += this.length;
+        for (const item of other) {
+            memcpy(<Opaque>ReferenceOf(newArray[index]), <Opaque>ReferenceOf(item[0]), sizeof(T) * item.length);
+            index += item.length;
+        }
+
+        return newArray;
+    }
 }
 
 static class Array {
