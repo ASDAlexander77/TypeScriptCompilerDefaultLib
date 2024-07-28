83ac66d3d73c376c5daf089ee8af326dbb9910d7
ASDAlexander77 <duzhar@googlemail.com>
2024-07-28 21:39:56 +0100

    fixes

--- src/generics/lib.generics.ts
+++ src/generics/lib.generics.ts
@@ -95,7 +95,7 @@ static class Array<T> {
 
     private clone(this: T[]) {
         var newArray = new Array<T>(this.length);
-        memcopy(<Opaque>ReferenceOf(newArray[0]), <Opaque>ReferenceOf(this[0]), sizeof(T) * this.length);
+        memcpy(<Opaque>ReferenceOf(newArray[0]), <Opaque>ReferenceOf(this[0]), sizeof(T) * this.length);
         return newArray;
     }
 }
