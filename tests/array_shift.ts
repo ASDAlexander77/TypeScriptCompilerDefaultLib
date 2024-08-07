37974e1eaf045ba66d6f2517c833e01bcbc6dbe3
ASDAlexander77 <duzhar@googlemail.com>
2024-08-07 18:17:40 +0100

    added test

---
+++ tests/array_shift.ts
@@ -0,0 +1,13 @@
+function main() {
+
+    let array1 = [1, 2, 3];
+
+    const firstElement = array1.shift();
+    
+    console.log(array1.length, array1);
+    // Expected output: Array [2, 3]
+    
+    console.log(firstElement);
+    // Expected output: 1
+    
+}
