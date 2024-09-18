92ed646bfc34ade09b02d0ea9d41b2f5262ec88d
ASDAlexander77 <duzhar@googlemail.com>
2024-09-18 23:54:35 +0000

    update build

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ "$1" == "debug" ] ; then	
-    ./scripts/build.sh debug
+    ./scripts/build.sh debug gcc pic
 else    
-    ./scripts/build.sh release
+    ./scripts/build.sh release gcc pic
 fi
