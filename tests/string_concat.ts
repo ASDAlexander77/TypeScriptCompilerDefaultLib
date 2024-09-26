5e2bc4f596d89d392c6046c24188d0544a9640d7
ASDAlexander77 <duzhar@googlemail.com>
2024-09-26 16:03:03 +0100

    update

--- tests/string_concat.ts
+++ tests/string_concat.ts
@@ -7,4 +7,17 @@ function main() {
 
   console.log(str2.concat(', ', str1));
   // Expected output: "World, Hello"
+
+  const hello = "Hello, ";
+  console.log(hello.concat("Kevin", ". Have a nice day."));
+  // Hello, Kevin. Have a nice day.
+
+  const greetList = ["Hello", " ", "Venkat", "!"];
+  "".concat(...greetList); // "Hello Venkat!"
+
+  "".concat({}); // "[object Object]"
+  "".concat([]); // ""
+  //"".concat(null); // "null"
+  "".concat(true); // "true"
+  "".concat(4, 5); // "45"  
 }
