2ec1674af65e45a4ca2b884f3652777353703cde
ASDAlexander77 <duzhar@googlemail.com>
2024-10-05 12:57:48 +0100

    added tests

---
+++ tests/string_replace.ts
@@ -0,0 +1,10 @@
+function main() {
+    const paragraph = "I think Ruth's dog is cuter than your dog!";
+
+    console.log(paragraph.replace("Ruth's", 'my'));
+    // Expected output: "I think my dog is cuter than your dog!"
+    
+    const regex = /Dog/i;
+    console.log(paragraph.replace(regex, 'ferret'));
+    // Expected output: "I think Ruth's ferret is cuter than your dog!"    
+}
---
+++ tests/string_replaceAll.ts
@@ -0,0 +1,11 @@
+function main() {
+    const paragraph = "I think Ruth's dog is cuter than your dog!";
+
+    console.log(paragraph.replaceAll('dog', 'monkey'));
+    // Expected output: "I think Ruth's monkey is cuter than your monkey!"
+    
+    // Global flag required when calling replaceAll with regex
+    const regex = /Dog/gi;
+    console.log(paragraph.replaceAll(regex, 'ferret'));
+    // Expected output: "I think Ruth's ferret is cuter than your ferret!"
+}
