c71ca9100b0159487313bb73b17cd7d2132e63f3
ASDAlexander77 <duzhar@googlemail.com>
2024-11-21 18:45:45 +0000

    update

--- tests/array_concat.ts
+++ tests/array_concat.ts
@@ -5,3 +5,6 @@ const newFruits = fruits.concat(["banana", "apple", "peach"]);
 
 for (const fruit of fruits) console.log("item = ", fruit);
 for (const fruit of newFruits) console.log("item = ", fruit);
+
+assert(fruits.length == 3)
+assert(newFruits.length == 6)
