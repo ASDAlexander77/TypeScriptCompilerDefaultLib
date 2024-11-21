0ea3a616121a45ddaebf6102f8bd94e20054758c
ASDAlexander77 <duzhar@googlemail.com>
2024-11-21 00:22:17 +0000

    removed all files

--- tests.bat
+++
@@ -1,7 +0,0 @@
-if "%1" == "" (
-	set BUILD=release
-) else (
-	set BUILD=debug
-)
-
-for /f %%f in ('dir /b .\tests\*.ts') do call scripts\test.bat %%~nf %BUILD%
---
+++ tests.cmd
@@ -0,0 +1,1 @@
+Powershell.exe -File tests.ps1
