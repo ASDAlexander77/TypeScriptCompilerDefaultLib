e515789c99e8fe50eb2c2ba38c52b7cd246a418c
ASDAlexander77 <duzhar@googlemail.com>
2026-07-09 23:56:59 +0000

    fix: update _sleep function parameter type from unsigned long to uint32_t

--- src/wrappers/thread.cpp
+++ src/wrappers/thread.cpp
@@ -1,7 +1,8 @@
 #include <chrono>
+#include <cstdint>
 #include <thread>
 
-extern "C" void _sleep(unsigned long milliseconds)
+extern "C" void _sleep(uint32_t milliseconds)
 {
     std::this_thread::sleep_for(std::chrono::milliseconds(milliseconds));
 }
