e3a8f41224a87f5300871a910e1b104e0225e13f
ASDAlexander77 <duzhar@googlemail.com>
2026-07-21 18:30:25 +0100

    fix: update build scripts to use tslang.exe and correct variable names

--- compiled.bat
+++ compiled.bat
@@ -1,6 +1,6 @@
-set BUILD_PATH=..\TypeScriptCompiler\__build\tsc\windows-msbuild-2026-debug
+set BUILD_PATH=..\TypeScriptCompiler\__build\tslang\windows-msbuild-2026-debug
 set TOOL_PATH=%BUILD_PATH%\bin
-set TSC_LIB_PATH=%BUILD_PATH%\lib
+set TSLANG_LIB_PATH=%BUILD_PATH%\lib
 set GC_LIB_PATH=..\TypeScriptCompiler\3rdParty\gc\x64\debug\lib
 set LLVM_LIB_PATH=..\TypeScriptCompiler\3rdParty\llvm\x64\debug\lib
-%TOOL_PATH%\tsc.exe  --nowarn --shared-libs=%TOOL_PATH%\TypeScriptRuntime.dll --default-lib-path=__build\debug --gc-lib-path=%GC_LIB_PATH% --tsc-lib-path=%TSC_LIB_PATH% --llvm-lib-path=%LLVM_LIB_PATH% --di --opt_level=0 --emit=exe %1
+%TOOL_PATH%\tslang.exe  --nowarn --shared-libs=%TOOL_PATH%\TypeScriptRuntime.dll --default-lib-path=__build\debug --gc-lib-path=%GC_LIB_PATH% --tslang-lib-path=%TSLANG_LIB_PATH% --llvm-lib-path=%LLVM_LIB_PATH% --di --opt_level=0 --emit=exe %1
